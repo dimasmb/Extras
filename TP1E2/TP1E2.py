cefcb4d87f7907faea1ed79dfda22648c9993618
Dimas <dbosch@itba.edu.ar>
2022-04-26 01:09:39 -0300

    Zo simulada en TP1E2

--- TP1E2/TP1E2.py
+++ TP1E2/TP1E2.py
@@ -16,14 +16,26 @@ RL = 7
 
 Zores=Zo(pot, RL)
 
+FILE_NAME1="F:\\ITBA\Años\\2022 1C\\E2\\Vvacio.txt"
+FILE_NAME2="F:\\ITBA\Años\\2022 1C\\E2\\Vcarga.txt"
+vacio = np.genfromtxt(FILE_NAME1, delimiter='\t', skip_header=True)
+carga = np.genfromtxt(FILE_NAME2, delimiter='\t', skip_header=True)
+x = vacio[:, 0]
+y_vacio = vacio[:, 1]
+y_carga = carga[:, 1]
+
+Zo_sim=(y_vacio-y_carga)*7/y_carga
+
 fig, ax = plt.subplots()
-ax.plot(pot/1000, Zores)
-ax.plot([0, 25], [3.12e-3, 1.26])
+ax.plot(pot/1000, Zores, label="Teórico")
+ax.plot(x/1000, Zo_sim, linestyle=" ", marker=".", label="Simulado")
 
 plt.xlim(0, 25)
+plt.ylim(0, 5)
 plt.xlabel(r'$R_{var} [k\Omega]$')
 plt.ylabel(r'$Z_{output} [\Omega]$')
 plt.title(r'$Z_{output}$' + " con " + r'$R_L=$' + str(RL) + r'$\Omega$')
 plt.ylim(bottom=0)
+plt.legend()
 plt.grid()
 plt.show()
