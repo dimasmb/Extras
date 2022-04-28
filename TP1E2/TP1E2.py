14b514eab8a56ef3bcea49a0299cb0c6124ffd30
dimas <dbosch@gmail.com>
2022-04-28 10:56:05 -0300

    medido

--- TP1E2/TP1E2.py
+++ TP1E2/TP1E2.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-T= lambda Rvar, RL: (400e6/(295*Rvar + 1999200)) * (1/(1 + 0.03*(1/Rvar + 1/RL)))
+T= lambda Rvar, RL: (2e6/(Rvar+2e6*(1+Rvar/6800)))*(200/(1+(75/2500)*(1/RL+1/Rvar)))
 
 Zia= lambda Rvar: 2e6/(295+1999200/Rvar)
-Zoa = lambda Rvar, RL: 75/(1+ (75*(1/Rvar+1/RL))/2500)
+Zoa = lambda Rvar, RL: 1/(1/Rvar + 1/RL)#75/(1+75*(1/Rvar + 1/RL))
 
 Zo = lambda Rvar, RL: Zoa(Rvar, RL)/(1 + T(Rvar, RL))
 
@@ -12,26 +12,27 @@ Zi = lambda Rvar, RL: Zia(Rvar)/(1 + T(Rvar, RL))
 
 
 pot = np.linspace(0.001, 25000, 10001)
-RL = 7
+RL = 8
 
 Zores=Zo(pot, RL)
 
-FILE_NAME1="F:\\ITBA\Años\\2022 1C\\E2\\Vvacio.txt"
-FILE_NAME2="F:\\ITBA\Años\\2022 1C\\E2\\Vcarga.txt"
-vacio = np.genfromtxt(FILE_NAME1, delimiter='\t', skip_header=True)
-carga = np.genfromtxt(FILE_NAME2, delimiter='\t', skip_header=True)
-x = vacio[:, 0]
-y_vacio = vacio[:, 1]
-y_carga = carga[:, 1]
+# FILE_NAME1="F:\\ITBA\Años\\2022 1C\\E2\\Vvacio.txt"
+# FILE_NAME2="F:\\ITBA\Años\\2022 1C\\E2\\Vcarga.txt"
+# vacio = np.genfromtxt(FILE_NAME1, delimiter='\t', skip_header=True)
+# carga = np.genfromtxt(FILE_NAME2, delimiter='\t', skip_header=True)
+# x = vacio[:, 0]
+# y_vacio = vacio[:, 1]
+# y_carga = carga[:, 1]
 
-Zo_sim=(y_vacio-y_carga)*7/y_carga
+# Zo_sim=(y_vacio-y_carga)*7/y_carga
 
 fig, ax = plt.subplots()
 ax.plot(pot/1000, Zores, label="Teórico")
-ax.plot(x/1000, Zo_sim, linestyle=" ", marker=".", label="Simulado")
+# ax.plot(x/1000, Zo_sim, linestyle=" ", marker=".", label="Simulado")
+ax.plot([20, 25], [0.69, 0.43], linestyle="--", linewidth=1, marker="x", markersize=5, label="Medido")
 
 plt.xlim(0, 25)
-plt.ylim(0, 5)
+# plt.ylim(0, 5)
 plt.xlabel(r'$R_{var} [k\Omega]$')
 plt.ylabel(r'$Z_{output} [\Omega]$')
 plt.title(r'$Z_{output}$' + " con " + r'$R_L=$' + str(RL) + r'$\Omega$')
